b604d9a0cf55b449e5e62070bbe376f0c7fce198
Fay Whittall <fxwhittall@wpi.edu>
2019-02-10 14:29:13 -0500

    A thing

--- part1.c
+++ part1.c
@@ -4,47 +4,88 @@
 #include <math.h>
 #include <time.h>
 #include <pthread.h>
-#include <sem.h>
+#include <semaphore.h>
+#include <unistd.h>
 
-void costumeDept(int isArr){
-  int numNinjas;
+typedef struct shopData_{
   int numPirates;
-  if(isArr){
-    sleep(getRandNormNum(avgArrPirate));
-  }
-  else sleep(getRandNormNum(avgArrNinja));
-  if(isArr){
-    while(numNinjas){
-      sem_wait(&hasPirates);
+  int numNinjas;
+  int numTeams;
+  double avgCostPirate, avgCostNinja, avgArrPirate, avgArrNinja;
+  sem_t *doorLock;
+  int ninjasInShop;
+  int piratesInShop;
+} shopData;
+
+double getRandNormNum(double avg);
+
+void costumeDept(int isArr, shopData* costumeShop){
+  int needsCostume = 1;
+  int isEntering = 0;
+  while(needsCostume){
+    //Aaaadventure time
+    if(isArr){
+      sleep(getRandNormNum(costumeShop->avgArrPirate));
     }
-    sem_post(&hasPirates);
+    else sleep(getRandNormNum(costumeShop->avgArrNinja));
+
+    //Walk up to the shop like what up I got a big sword
+    while(!isEntering){
+      sem_wait(costumeShop->doorLock);
+      if(isArr){
+        if(!costumeShop->ninjasInShop){
+          costumeShop->piratesInShop++;
+          isEntering = 1;
+        }
+        sem_post(costumeShop->doorLock);
+      }
+      else{
+        if(!costumeShop->piratesInShop){
+          costumeShop->ninjasInShop++;
+          isEntering = 1;
+        }
+        sem_post(costumeShop->doorLock);
+      }
+    }
+
+    //if there is an available team, go into the costume shop
     sem_wait(&teams);
-    sleep(getRandNormNum(avgCostPirate));
+    if(isArr){
+      sleep(getRandNormNum(costumeShop->avgCostPirate));
+    }
+    else sleep(getRandNormNum(costumeShop->avgCostNinja));
     sem_post(&teams);
 
-  }
-  else{
-    while(numPirates){
-      sem_wait(&hasNinjas);
+    //leave the costume shop
+    sem_wait(costumeShop->doorLock);
+    if(isArr){
+      costumeShop->piratesInShop--;
     }
-    sem_post(&hasPirates);
-    sem_wait(&teams);
-    sleep(getRandNormNum(avgCostNinja));
-    sem_post(&teams);
+    else costumeShop->ninjasInShop--;
+    sem_wait(costumeShop->doorLock);
+
+    //let there be a 25% chance that the pirate or ninja will return to the costume shop
+    if(!rand()%4){
+      needsCostume = 0;
+    }
   }
 }
 
-double getRandNormNum(int avg){
-  return avg*(sqrt(-2 * log(drand48())) * cos(2 * M_PI * drand48()) + 1);
+double getRandNormNum(double avg){
+  double num = avg*(sqrt(-2 * log(drand48())) * cos(2 * M_PI * drand48()) + 1);
+  while(num < 0){
+    num = avg*(sqrt(-2 * log(drand48())) * cos(2 * M_PI * drand48()) + 1);
+  }
+  return num;
 }
 
 int main(int argc, char* argv[]){
-  sem_t hasPirates;
-  sem_t hasNinjas;
   sem_t teams;
-
+  srand(time(0));
   srand48(time(0));
-  int numTeams, numPirates, numNinjas, avgCostPirate, avgCostNinja, avgArrPirate, avgArrNinja;
+  int numTeams, numPirates, numNinjas;
+  double avgCostPirate, avgCostNinja, avgArrPirate, avgArrNinja;
+  shopData *costumeShop;
   if(argc != 8){ //incorrect number of arguments
     printf("Incorrect argument entry! Arguments are are follows:\n");
     printf("\t ./part1 <number of teams> <number of pirates> <number of ninjas> <average pirate costuming time> <average ninja costuming time> <average pirate arrival time> <average ninja arrival time>\n");
@@ -65,16 +106,34 @@ int main(int argc, char* argv[]){
     printf("Error! There can be between 10 and 50 ninjas.\n");
     return 1;
   }
-  avgCostPirate = atoi(argv[4]);
-  avgCostNinja = atoi(argv[5]);
-  avgArrPirate = atoi(argv[6]);
-  avgArrNinja = atoi(argv[7]);
+  avgCostPirate = atof(argv[4]);
+  avgCostNinja = atof(argv[5]);
+  avgArrPirate = atof(argv[6]);
+  avgArrNinja = atof(argv[7]);
 
-  sem_init(&hasPirates, 0, 0);
-  sem_init(&hasNinjas, 0, 0);
+  *costumeShop = (shopData*) malloc(sizeof(shopData));
+
+  costumeShop->numPirates = numPirates;
+  costumeShop->numNinjas = numNinjas;
+  costumeShop->numTeams = numTeams;
+  costumeShop->ninjasInShop = 0;
+  costumeShop->piratesInShop = 0;
+  costumeShop->avgArrNinja = avgArrNinja;
+  costumeShop->avgArrPirate = avgArrPirate;
+  costumeShop->avgCostNinja = avgCostNinja;
+  costumeShop->avgCostPirate = avgCostPirate;
+
   sem_init(&teams, 0, numTeams);
+  sem_init(costumeShop->doorLock, 0, 1);
 
-  printf("%f\n", getRandNormNum(avgCostPirate));
+  for(int i = 0; i < numPirates; i++){
+    pthread_t pirate;
+    pthread_create(pirate, NULL, costumeDept, 1, NULL);
+  }
+  for(int i = 0; i < numNinjas; i++){
+    pthread_t ninja;
+    pthread_create(ninja, NULL, costumeDept, 0, NULL);
+  }
 
   return 0;
 }
